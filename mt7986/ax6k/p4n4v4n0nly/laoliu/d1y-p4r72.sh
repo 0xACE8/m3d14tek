ec0eae121acf35d07674bda76bb59ff5c56517a8
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-10-18 01:42:50 -0700

    Update d1y-p4r72.sh

--- mt7986/ax6k/p4n4v4n0nly/laoliu/d1y-p4r72.sh
+++ mt7986/ax6k/p4n4v4n0nly/laoliu/d1y-p4r72.sh
@@ -27,7 +27,7 @@ sed -i 's\[ -f /etc/banner ] && cat /etc/banner\[ -f /etc/banner ] && cat /etc/b
 rm -rf package/base-files/files/etc/banner
 wget --no-check-certificate -O package/base-files/files/etc/banner "https://raw.githubusercontent.com/0xACE8/m3d14tek/refs/heads/main/mt7986/ax6k/p4n4v4n0nly/laoliu/banner"
 wget --no-check-certificate -O feeds/packages/utils/bash/files/etc/profile.d/30-sysinfo.sh "https://raw.githubusercontent.com/0xACE8/0p3nwrt-general/refs/heads/main/30-sysinfo.sh"
-rm -rf feeds/ace8/luci-theme-kucat/htdocs/luci-static/kucat/img/bg1.jpg
-wget --no-check-certificate -O feeds/ace8/luci-theme-kucat/htdocs/luci-static/kucat/img/bg1.jpg "https://raw.githubusercontent.com/0xACE8/m3d14tek/refs/heads/main/mt7986/ax6k/p4n4v4n0nly/laoliu/bg1.jpg"
+#rm -rf feeds/ace8/luci-theme-kucat/htdocs/luci-static/kucat/img/bg1.jpg
+#wget --no-check-certificate -O feeds/ace8/luci-theme-kucat/htdocs/luci-static/kucat/img/bg1.jpg "https://raw.githubusercontent.com/0xACE8/m3d14tek/refs/heads/main/mt7986/ax6k/p4n4v4n0nly/laoliu/bg1.jpg"
 
 
