1e65df757f8813ac3f563166bf81b3e74ff35bf3
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-10-20 03:13:25 -0700

    Create d1y-p4r71.sh

---
+++ mt7981/rax3kz/laoliu/d1y-p4r71.sh
@@ -0,0 +1,4 @@
+#---------Ace Sourse-----
+sed -i '1 i\src-git ace8 https://github.com/0xACE8/4c38-p4ck463;laoliu' feeds.conf.default
+sed -i '2 i\src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default\
+sed -i '1 i\src-git v5 https://github.com/sbwml/openwrt_helloworld;v5' feeds.conf.default
