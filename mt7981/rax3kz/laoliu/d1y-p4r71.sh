44a1c02dfc74cab48b183f3fdf1f58c19f55ca40
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-10-20 07:52:59 -0700

    Update d1y-p4r71.sh

--- mt7981/rax3kz/laoliu/d1y-p4r71.sh
+++ mt7981/rax3kz/laoliu/d1y-p4r71.sh
@@ -2,3 +2,7 @@
 sed -i '1 i\src-git ace8 https://github.com/0xACE8/4c38-p4ck463;laoliu' feeds.conf.default
 sed -i '2 i\src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default\
 sed -i '1 i\src-git v5 https://github.com/sbwml/openwrt_helloworld;v5' feeds.conf.default
+
+# patch
+rm -rd target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dtsi
+wget --no-check-certificate -O target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dtsi "https://raw.githubusercontent.com/0xACE8/m3d14tek/main/mt7981/rax3kz/laoliu/mt7981-cmcc-rax3000m.dtsi"
