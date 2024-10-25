a5665bf5a3c4601104f3725542b04eb3fa174da4
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-10-25 04:11:53 -0700

    Update d1y-p4r71.sh

--- mt7986/ax6k/p4n4v4n0nly/laoliu/d1y-p4r71.sh
+++ mt7986/ax6k/p4n4v4n0nly/laoliu/d1y-p4r71.sh
@@ -1,8 +1,8 @@
 #---------Ace Sourse-----
 sed -i '1 i\src-git ace8 https://github.com/0xACE8/4c38-p4ck463;laoliu' feeds.conf.default
-sed -i '2 i\src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default\
-sed -i '1 i\src-git v5 https://github.com/sbwml/openwrt_helloworld;v5' feeds.conf.default
-sed -i '1 i\src-git small https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '2 i\src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
+sed -i '$a src-git v5 https://github.com/sbwml/openwrt_helloworld;v5' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
 
 ## 修改DTS的ubi为490MB的0x1ea00000
 sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000.dts
