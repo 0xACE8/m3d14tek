1c7622daeb5c47deeb3fa797bb0c0425ad99da98
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-12-13 20:20:36 -0800

    Update d1y-p4r71.sh

--- mt7986/ax6k/lede/d1y-p4r71.sh
+++ mt7986/ax6k/lede/d1y-p4r71.sh
@@ -1,5 +1,5 @@
 #---------Ace Sourse-----
-sed -i '1 i\src-git ace8 https://github.com/0xACE8/4c38-p4ck463;main' feeds.conf.default
+sed -i '1 i\src-git ace8 https://github.com/0xACE8/4c38-p4ck463;18.06' feeds.conf.default
 sed -i '2 i\src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
 
 ## 修改DTS的ubi为490MB的0x1ea00000
