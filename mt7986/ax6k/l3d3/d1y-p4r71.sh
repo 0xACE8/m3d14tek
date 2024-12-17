d22dce18b3325e256cefc4c83f940465c7795c44
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-12-16 23:55:44 -0800

    Update d1y-p4r71.sh

--- mt7986/ax6k/l3d3/d1y-p4r71.sh
+++ mt7986/ax6k/l3d3/d1y-p4r71.sh
@@ -1,6 +1,6 @@
 #---------Ace Sourse-----
 sed -i '1 i\src-git ace8 https://github.com/0xACE8/4c38-p4ck463;main' feeds.conf.default
-sed -i '2 i\src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
+sed -i '2 i\src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' feeds.conf.default
 
 ## 修改DTS的ubi为490MB的0x1ea00000
 sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/dts/mt7986a-xiaomi-redmi-router-ax6000.dts
